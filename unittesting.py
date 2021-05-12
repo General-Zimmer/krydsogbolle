cf06be04df03b80d8bea14f8acf05ed91515768f
GZimmer <kristianz1000@gmail.com>
2021-05-12 12:23:13 +0200

    unittest

--- unittesting.py
+++ unittesting.py
@@ -1,13 +1,13 @@
 import unittest
 from tkinter import *
 import logic as logic
-import data as data
-import graphics as gfx
+from data import *
+from graphics import *
 
 
 class DataTests(unittest.TestCase):
     def setUp(self):
-        self.data = data.data()
+        self.data = data()
 
     def test_setscore(self):
         pass
@@ -19,7 +19,7 @@ class DataTests(unittest.TestCase):
 class LogiTests(unittest.TestCase):
     def setUp(self):
         root = Tk()
-        self.gfx = gfx.gui(root)
+        self.gfx = GameFrame(root)
 
     def test_checkwin1(self):
         self.gfx.logi.SetScore("bolle", 0)
@@ -58,3 +58,21 @@ class LogiTests(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+
+class DatabaseTests(unittest.TestCase):
+    def setUp(self):
+        self.sql = mysql_connector()
+
+    # Da vi ikke ved hvordan vi kan teste hver database handling uden brugen af en add og delete, så valgte vi at lave
+    # det hele i en test modul
+    def test_all(self):
+        self.sql.add("testing da test test", "yeet")
+        self.sql.modify("testing da test test", "meat")
+        row = self.sql.pull("testing da test test")
+        self.assertEqual(row[1], "meat")
+
+        self.sql.delete("testing da test test")
+
+    def tearDown(self):
+        self.sql.close()
